2642eb8aa30e495ecffe736490e86af501a282fb
itggot-adin-smailhodzic <adin.smailhodzic@itggot.se>
2018-05-03 23:23:17 +0200

    Slutfört
    Projektkod fullständig. Motivering bör finnas på projektbeskrivningen & utvecklingsdagboken.

--- app.rb
+++ app.rb
@@ -24,17 +24,11 @@ class App < Sinatra::Base
 	end
 
 	post '/register_user' do
-		db = SQLite3::Database.new("db/database.db")
-
 		username = params[:username]
 		password = params[:password]
 
-		password_digest = BCrypt::Password.create(password)
+		register(username,password)
 
-
-		# 'State' = 0; Offline. 'State' = 1; Online. 'State' = 2; Away.
-
-		db.execute("INSERT INTO Users(username,password,state) VALUES(?,?,0)", [username,password_digest])
 		redirect('/')
 	end
 
@@ -45,48 +39,37 @@ class App < Sinatra::Base
 		username = params[:username]
 		password = params[:password]
 
-		id, username_verify, password_verify, state = db.execute("SELECT * FROM Users WHERE username = '#{username}'")[0]
+		user_info = login_verify(username,password)
 
-		if password_verify != nil
-			password_verify = BCrypt::Password.new(password_verify)
+		if user_info[2] != nil
+			user_info[2] = BCrypt::Password.new(user_info[2])
 		else
-			password_verify = ""
+			user_info[2] = ""
 		end
 
-		if username == username_verify && password_verify == password
+		if username == user_info[1] && user_info[2] == password
 			# Login successful
-			session[:id] = id
+			session[:id] = user_info[0]
 			session[:username] = username
 			redirect('/profile/' + session[:id].to_s)
 		else
-			slim :error
+			redirect('/error')
 		end
 
 	end
 
-	post '/password_recovery' do
-		
-	end
-
 	post '/accept_friend' do
-
 		id = params[:table_id].to_i
-
-		db = SQLite3::Database.new('db/database.db')
-		db.execute("UPDATE Relations SET Relation_State = 1 WHERE ID = #{id}")
+		accept_friend(id)
 
 		redirect('/profile/' + session[:id].to_s)
-
 	end
+
 	post '/decline_friend' do
-
 		id = params[:table_id].to_i
-
-		db = SQLite3::Database.new('db/database.db')
-		db.execute("DELETE FROM Relations WHERE ID = #{id}")
+		decline_friend(id)
 
 		redirect('/profile/' + session[:id].to_s)
-
 	end
  
 	get '/profile/:id' do
@@ -99,25 +82,9 @@ class App < Sinatra::Base
 
 		session[:id] = id
 
-		db = SQLite3::Database.new('db/database.db')
+		result = list_friends(id)
 
-		result = db.execute("SELECT * FROM Relations WHERE (User_1 = #{session[:id]} OR User_2 = #{session[:id]})")
-
-		# result_string = result.to_s
-
-		# index = 0
-		# values = []
-		  
-		# result_string.each_char do |i|
-		#   if i == '>'[0] then
-		# 	values << index + 1
-		#   end
-		#   index += 1
-		# end
-
-		var = 0 
-
-		slim(:menu, locals:{notes:result, id:id, i:var})  
+		slim(:menu, locals:{notes:result, id:id, i:0})  
 	end
 
 	post '/friend_request' do
@@ -138,57 +105,53 @@ class App < Sinatra::Base
 
 		# 0 = Pending, 1 = Accepted, 2 = Denied, 3 = Blocked
 		
-		user_reciever_id_instance = db.execute("SELECT * FROM Users WHERE id = ?", [User_reciever])
+		request_sent = friend_request(user_sender,user_reciever)
 
-		if user_reciever_id_instance.empty? == false # Checks if the user recipent is non-existant
-
-			if User_sender < User_reciever
-				db.execute("INSERT INTO Relations(User_1,User_2,Relation_State,User_Action) VALUES(?,?,0,?)", [User_sender, User_reciever, User_sender])
-			elsif User_reciever < User_sender
-				db.execute("INSERT INTO Relations(User_1,User_2,Relation_State,User_Action) VALUES(?,?,0,?)", [User_reciever, User_sender, User_sender])
-			else
-				redirect('/error')
-			end
-	
+		if request_sent == true
 			redirect('/profile/' + session[:id].to_s)
-
-		else
-
+		elsif request_sent == false
 			redirect('/error')
-
 		end
 
 	end
-	
+
 	get '/forum' do
+		posts =	forum_load()
 
-		db = SQLite3::Database.new("db/database.db")
-
-		posts =	db.execute('SELECT * FROM Posts WHERE parent_id IS NULL')
-		
 		slim(:forum,locals:{posts:posts})
-
 	end
 
 	get '/post/:id' do
-
 		post_id = params[:id].to_i
-
-		parent_post = urmom(post_id)
-		
-		child_posts = db.execute('SELECT * FROM Posts WHERE parent_id IS ?', [post_id])
-	
-
-		slim(:post, locals:{parent_post:parent_post, child_posts:child_posts})
-
+		posts = post_and_comments(post_id)
+			
+		slim(:post, locals:{parent_post:posts[0], child_posts:posts[1]})
 	end
 
 	post '/create_post' do
 
 		user_id = session[:id]
+		title = params[:create_post_title]
+		content = params[:create_post_content]
+		is_comment = params[:is_comment].to_i
 
-		create_post(user_id)
 
+		if user_id == nil
+			redirect('/error')
+		else
+			if is_comment == 0
+				create_post(title,content,user_id)
+			else
+				parent_id = params[:parent_id]
+				create_comment(title,content,user_id,parent_id)
+			end
+			redirect('/forum')
+		end
+
+	end
+
+	get '/error' do
+		slim :error
 	end
 
 	#	Websocket application - Generic Chatroom
--- module.rb
+++ module.rb
@@ -5,16 +5,74 @@ module GModule
         return db
     end
 
-    def urmom(post_id)
-        db = db_connect()
-        parent_post = db.execute('SELECT * FROM Posts WHERE id IS ?', [post_id])
-        return parent_post
+    def register(username,password)
+        db = db.connect()
+        db.execute("INSERT INTO Users(username,password,state) VALUES(?,?,0)", [username,password_digest])
     end
 
-    def create_post(user_id)
-        db = db_connect
+    def login_verify(username,password)
+        db = db_connect()
+        id, username_verify, password_verify, state = db.execute("SELECT * FROM Users WHERE username = '#{username}'")[0]
+        
+        return [id,username_verify,password_verify,state] 
+    end
 
+    def accept_friend(id)
+        db = db_connect()
+		db.execute("UPDATE Relations SET Relation_State = 1 WHERE ID = ?", [id])
+    end
 
+    def decline_friend(id)
+        db = db_connect()
+        db.execute("DELETE FROM Relations WHERE ID = ?", [id])
+    end
+
+    def list_friends(id)
+        db = db_connect()
+        return db.execute("SELECT * FROM Relations WHERE (User_1 = #{session[:id]} OR User_2 = #{session[:id]})")
+    end
+
+    def friend_request(user_sender,user_reciever)
+        db = db.connect()
+        user_reciever_id_instance = db.execute("SELECT * FROM Users WHERE id = ?", [user_reciever])
+
+		if user_reciever_id_instance.empty? == false # Checks if the user recipent is non-existant
+
+			if user_sender < user_reciever
+				db.execute("INSERT INTO Relations(User_1,User_2,Relation_State,User_Action) VALUES(?,?,0,?)", [user_sender, user_reciever, user_sender])
+			elsif user_reciever < user_sender
+				db.execute("INSERT INTO Relations(User_1,User_2,Relation_State,User_Action) VALUES(?,?,0,?)", [user_reciever, user_sender, user_sender])
+			else
+				return false
+            end
+            return true
+		else
+			return false
+		end
+    end
+
+    def forum_load()
+        db = db_connect()
+        db.execute('SELECT * FROM Posts WHERE parent_id IS NULL')
+    end
+
+    def post_and_comments(post_id)
+        db = db_connect()
+        parent_post = db.execute('SELECT * FROM Posts WHERE id IS ?', [post_id])
+        child_posts = db.execute('SELECT * FROM Posts WHERE parent_id IS ?', [post_id])
+	
+        return [parent_post, child_posts]
+    end
+
+    def create_post(title,content,user_id)
+        db = db_connect()
+        db.execute('INSERT INTO Posts(title,content,user_id,parent_id,upvotes,downvotes) VALUES(?,?,?,NULL,0,0)', [title,content,user_id])
+    end
+
+    def create_comment(title,content,user_id,parent_id)
+        db = db_connect()
+        db.execute('INSERT INTO Posts(title,content,user_id,parent_id,upvotes,downvotes) VALUES(?,?,?,?,0,0)', [title,content,user_id,parent_id])
+    end
 end
 
 
